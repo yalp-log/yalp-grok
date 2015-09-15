20dec9772c314b11093859ea776d2ff4b2cf2255
Timothy Messier <tim.messier@gmail.com>
2015-09-15 11:33:41 -0400

    add version.py

---
+++ yalp_grok/version.py
@@ -0,0 +1,16 @@
+# vim: set et ts=4 sw=4 fileencoding=utf-8:
+'''
+yalp_grok.version
+=================
+
+Get version of yalp_grok
+'''
+from __future__ import print_function
+
+from distutils.version import StrictVersion  # pylint: disable=E0611,F0401
+__version__ = str(StrictVersion('0.1'))
+del StrictVersion
+
+
+if __name__ == '__main__':  # pragma: no cover
+    print(__version__)
